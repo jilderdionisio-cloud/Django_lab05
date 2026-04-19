11f472c9454c44678dbd85b55e31e1b8d1ec1551
Jilder Dionisio <jilder.dionisio@tecsup.edu.pe>
2026-04-18 19:30:22 -0500

    feat: implementar endpoints CRUD para modelo Movie

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+}
